9a61f4ab4d5483890c90fe6ff518ea125dfac799
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-01-31 06:19:53 +0300

    added aside right

--- src/components/AsideRight/AsideRight.tsx
+++ src/components/AsideRight/AsideRight.tsx
@@ -1,11 +1,11 @@
-const Right = () => {
+const AsideRight = () => {
   return (
     // Right Side
     <aside
       id="right"
-      className="sm:hidden md:block md:w-80 lg:w-96 shadow-gray-200 shadow-lg rounded-tr-lg bg-white"
+      className="hidden lg:block lg:w-80 xl:w-96 shadow-gray-200 shadow-lg rounded-tr-lg bg-white"
     ></aside>
   );
 };
 
-export default Right;
+export default AsideRight;
