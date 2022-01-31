4435d14b6a33b20cd51c59d9d9401437b034e952
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-01-31 06:19:21 +0300

    added aside left

--- src/components/AsideRight/AsideRight.tsx
+++ src/components/AsideRight/AsideRight.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+const Right = () => {
+  return (
+    // Right Side
+    <aside
+      id="right"
+      className="sm:hidden md:block md:w-80 lg:w-96 shadow-gray-200 shadow-lg rounded-tr-lg bg-white"
+    ></aside>
+  );
+};
 
-interface Props {}
-
-function AsideRight(props: Props) {
-  return <aside className="flex-none w-80">aside right</aside>;
-}
-
-export default AsideRight;
+export default Right;
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,10 @@
-import { PlusCircleIcon } from "@heroicons/react/solid";
+import {
+  PlusCircleIcon,
+  BadgeCheckIcon,
+  TrashIcon,
+  CheckIcon,
+} from "@heroicons/react/solid";
+import { CalendarIcon } from "@heroicons/react/outline";
 
 const Main = () => {
   return (
@@ -17,8 +23,39 @@ const Main = () => {
           </button>
         </div>
       </header>
-      <main className="bg-white m-3 px-3 py-5 shadow-lg shadow-gray-100 rounded-md">
-        main
+      <main className="card-white">
+        <div className="tasks flex flex-col items-center gap-3 w-full">
+          <div
+            id="task"
+            className="card-white  max-w-lg w-full shadow  border-l-4 border-blue-500 flex justify-between relative"
+          >
+            <div className="flex flex-col gap-3">
+              <h5 className="font-semibold tracking-wider text-green-500">
+                Trip
+              </h5>
+              <h1 className="text-lg font-bold tracking-wider">
+                Learnig Lesson
+              </h1>
+              <hr />
+              <div className="flex items-center gap-2 ml-3 ">
+                <BadgeCheckIcon className="w-7 h-7 text-green-500 rounded-full"></BadgeCheckIcon>
+                <span className="text-green-500 font-semibold tracking-wide">
+                  Complete
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-col justify-around items-center">
+              <div className="flex gap-2">
+                <span className="text-sm text-slate-500"> Apr 12</span>
+                <CalendarIcon className="w-5 h-5 text-slate-500 rounded-full"></CalendarIcon>
+              </div>
+              <div className="flex justify-between w-14 h-5">
+                <CheckIcon className="w-5 h-5 text-green-500 cursor-pointer rounded-full"></CheckIcon>
+                <TrashIcon className="w-5 h-5 text-red-500  cursor-pointer rounded-full"></TrashIcon>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </section>
   );
