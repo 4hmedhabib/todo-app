ef093a48330673de9225809cd3e83fed549f44af
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-03-04 11:21:20 +0300

    added

--- src/components/Main/Header.tsx
+++ src/components/Main/Header.tsx
@@ -2,12 +2,13 @@ const Header = () => {
   return (
     // Header
     <header className="m-4 flex items-center justify-between">
-      <div id="logo-text">
+      <div id="logo-text" className="md:hidden">
         <h1 className="text-2xl font-bold tracking-wider">Tiigsi Todo App</h1>
         <p className="text-sm text-gray-500 tracking-wider">
           Save Your Tasks in Tiigsi Todo
         </p>
       </div>
+      <div></div>
     </header>
   );
 };
